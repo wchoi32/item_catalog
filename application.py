6e57bafef7934bb37b8a1bc7a02d864de32b77de
WooSung Choi <johnwschoi@gmail.com>
2018-07-14 22:19:24 -0700

    working router for homepage, category items, item description, set up templates. Now implementing login feature

--- application.py
+++ application.py
@@ -1,16 +1,56 @@
-
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from sqlalchemy import create_engine
+app = Flask(__name__)
+
+from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CategoryItem
 
-app = Flask(__name__)
+from flask import session as login_session
+import random
+import string
 
 engine = create_engine('sqlite:///itemcatalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+@app.route('/')
+@app.route('/catalog/')
+def show_catalogs():
+    categories = session.query(Category).order_by(asc(Category.name))
+    latest_items = session.query(CategoryItem, Category).join(
+        Category, CategoryItem.cat_id == Category.id).order_by(desc(CategoryItem.id))
+
+    return render_template('latestItems.html',
+                           categories=categories,
+                           latest_items=latest_items)
+
+
+@app.route('/catalog/<path:catalog_name>/')
+@app.route('/catalog/<path:catalog_name>/items')
+def show_catalog_items(catalog_name):
+    categories = session.query(Category).order_by(asc(Category.name))
+    category_selected = session.query(
+        Category).filter_by(name=catalog_name).one()
+    category_items = session.query(
+        CategoryItem).filter_by(cat_id=category_selected.id).all()
+    category_count = session.query(
+        CategoryItem).filter_by(cat_id=category_selected.id).count()
+    return render_template('categoryItems.html',
+                           categories=categories,
+                           category_count=category_count,
+                           category_name=catalog_name,
+                           category_items=category_items)
+
+
+@app.route('/catalog/<path:catalog_name>/<path:item_name>')
+def item_description(catalog_name, item_name):
+    item_description = session.query(
+        CategoryItem).filter_by(title=item_name).one()
+    return render_template('itemDescription.html',
+                           item_description=item_description)
 
 
 @app.route('/catalog.json')
@@ -28,6 +68,14 @@ def catalog_json():
 
         all_categories_items.append(each_catalog)
     return jsonify(Category=all_categories_items)
+
+
+@app.route('/login')
+def login():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
 
 
 if __name__ == '__main__':
