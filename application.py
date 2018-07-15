f614886b6d8d46311214be1d367bdc5031bb55ea
WooSung Choi <johnwschoi@gmail.com>
2018-07-15 13:56:01 -0700

    working version of oauth google login, creater user db, modify seed data to fit

--- application.py
+++ application.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, CategoryItem
-
+from database_setup import Base, Category, CategoryItem, User
 from flask import session as login_session
 import random
 import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Item Catalog App"
 
 engine = create_engine('sqlite:///itemcatalog.db')
 Base.metadata.bind = engine
@@ -19,9 +28,9 @@ session = DBSession()
 @app.route('/')
 @app.route('/catalog/')
 def show_catalogs():
-    categories = session.query(Category).order_by(asc(Category.name))
+    categories = session.query(Category).order_by(asc(Category.name)).all()
     latest_items = session.query(CategoryItem, Category).join(
-        Category, CategoryItem.cat_id == Category.id).order_by(desc(CategoryItem.id))
+        Category, CategoryItem.cat_id == Category.id).order_by(desc(CategoryItem.id)).all()
 
     return render_template('latestItems.html',
                            categories=categories,
@@ -31,13 +40,14 @@ def show_catalogs():
 @app.route('/catalog/<path:catalog_name>/')
 @app.route('/catalog/<path:catalog_name>/items')
 def show_catalog_items(catalog_name):
-    categories = session.query(Category).order_by(asc(Category.name))
+    categories = session.query(Category).order_by(asc(Category.name)).all()
     category_selected = session.query(
         Category).filter_by(name=catalog_name).one()
     category_items = session.query(
         CategoryItem).filter_by(cat_id=category_selected.id).all()
     category_count = session.query(
         CategoryItem).filter_by(cat_id=category_selected.id).count()
+
     return render_template('categoryItems.html',
                            categories=categories,
                            category_count=category_count,
@@ -78,6 +88,147 @@ def login():
     return render_template('login.html', STATE=state)
 
 
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    request.get_data()
+    code = request.data.decode('utf-8')
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    response = h.request(url, 'GET')[1]
+    str_response = response.decode('utf-8')
+    result = json.loads(str_response)
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print("Token's client ID does not match app's.")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['email'] = data['email']
+
+    # See if user exists, if not make a new one
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    flash("you are now logged in as %s" % login_session['username'])
+    return output
+
+# User Helper Functions
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'],
+                   email=login_session['email'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print('Access Token is None')
+        response = make_response(json.dumps(
+            'Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        response = make_response(json.dumps(
+            'Successfully disconnected. '), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps(
+            'Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
 if __name__ == '__main__':
-    app.debug = True
+    app.secret_key = 'secret_key'
+    app.debug = False
     app.run(host='0.0.0.0', port=3000)
--- seed_data.py
+++ seed_data.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Category, Base, CategoryItem
+from database_setup import Base, Category, CategoryItem, User
 
 engine = create_engine('sqlite:///itemcatalog.db')
 
@@ -12,95 +12,110 @@ session = DBSession()
 
 session.query(Category).delete()
 session.query(CategoryItem).delete()
+session.query(User).delete()
 
-category1 = Category(name="Soccer")
+user1 = User(name="john",
+             email="test@test.com")
+session.add(user1)
+session.commit()
+
+category1 = Category(name="Soccer", user=user1)
 session.add(category1)
 session.commit()
 
-category2 = Category(name="Basketball")
+category2 = Category(name="Basketball", user=user1)
 session.add(category2)
 session.commit()
 
-category3 = Category(name="Baseball")
+category3 = Category(name="Baseball", user=user1)
 session.add(category3)
 session.commit()
 
-category4 = Category(name="Frisbee")
+category4 = Category(name="Frisbee", user=user1)
 session.add(category4)
 session.commit()
 
-category5 = Category(name="Snowboarding")
+category5 = Category(name="Snowboarding", user=user1)
 session.add(category5)
 session.commit()
 
-category6 = Category(name="Rock Climbing")
+category6 = Category(name="Rock Climbing", user=user1)
 session.add(category6)
 session.commit()
 
-category7 = Category(name="Foosball")
+category7 = Category(name="Foosball", user=user1)
 session.add(category7)
 session.commit()
 
-category8 = Category(name="Skating")
+category8 = Category(name="Skating", user=user1)
 session.add(category8)
 session.commit()
 
-category9 = Category(name="Hockey")
+category9 = Category(name="Hockey", user=user1)
 session.add(category9)
 session.commit()
 
 
 categoryItem1 = CategoryItem(title="Soccer Cleats",
                              description="The shoes",
-                             category=category1)
+                             category=category1,
+                             user=user1)
 session.add(categoryItem1)
 session.commit()
 
 categoryItem2 = CategoryItem(title="Jersey",
                              description="the shirt",
-                             category=category1)
+                             category=category1,
+                             user=user1)
 session.add(categoryItem2)
 session.commit()
 
 categoryItem3 = CategoryItem(title="Orange Ball",
                              description="basketball",
-                             category=category2)
+                             category=category2,
+                             user=user1)
 session.add(categoryItem3)
 session.commit()
 
 categoryItem4 = CategoryItem(title="Jordan",
                              description="type of shoes",
-                             category=category2)
+                             category=category2,
+                             user=user1)
 session.add(categoryItem4)
 session.commit()
 
 categoryItem5 = CategoryItem(title="Stadium",
                              description="live stadium",
-                             category=category2)
+                             category=category2,
+                             user=user1)
 session.add(categoryItem5)
 session.commit()
 
 categoryItem6 = CategoryItem(title="Bat",
                              description="baseball bat",
-                             category=category3)
+                             category=category3,
+                             user=user1)
 session.add(categoryItem6)
 session.commit()
 
 categoryItem7 = CategoryItem(title="Baseball Cleets",
                              description="baseball shoes",
-                             category=category3)
+                             category=category3,
+                             user=user1)
 session.add(categoryItem7)
 session.commit()
 
 categoryItem8 = CategoryItem(title="glove",
                              description="baseball glove",
-                             category=category3)
+                             category=category3,
+                             user=user1)
 session.add(categoryItem8)
 session.commit()
 
 categoryItem9 = CategoryItem(title="base",
                              description="there are three bases, known as first, second, and thirdbase",
-                             category=category3)
+                             category=category3,
+                             user=user1)
 session.add(categoryItem9)
 session.commit()
 
